17fa59ae4a5c2e496b66096fd145326cd2182860
alister.amo <alister.amo@eurecat.org>
2016-07-22 01:04:52 +0200

    cookie decrypt code merge

--- firefox_decrypt.py
+++ firefox_decrypt.py
@@ -23,7 +23,6 @@ import logging
 import os
 import sqlite3
 import sys
-import time
 from base64 import b64decode
 from ctypes import c_uint, c_void_p, c_char_p, cast, byref, string_at
 from ctypes import Structure, CDLL
@@ -679,17 +678,95 @@ def main():
 
     # And finally decode all passwords
     nss.decrypt_passwords(profile, password, args.export_pass)
+    
+    # ADDED - try to get cookies using incorporated code from decryptcookies and the parameters we already obtained
+    decryptcookies(profile + "\cookies.sqlite", profile + "\plaincookies.txt")
+    
+# ADDED - decrypt cookies code
+
+''' Given a location to firefox cookie sqlite file
+    Write its date param - expiry, last accessed,
+    Creation time to a file in plain text.
+    id
+    baseDomain
+    appId
+    inBrowserElement
+    name
+    value
+    host
+    path
+    expiry
+    lastAccessed
+    creationTime
+    isSecure
+    isHttpOnly
+    python /home/daniel/python/cookie_viewer.py $(find /home/daniel/ -type f -name 'cookies.sqlite' | head -1) /tmp/test.txt 
+'''
+ 
+import sys
+import os
+from datetime import datetime
+import sqlite3
+ 
+def Usage():
+    print "{0} cookie-fullpath output-file".format(sys.argv[0])
+    sys.exit(1)
+ 
+def convert(epoch):
+    # Some dates in the cookies file might not be valid, or too big
+    MAXDATE=2049840000
+    mydate=epoch[:10]
+    if int(mydate)>MAXDATE:
+        mydate=str(MAXDATE)
+    if len(epoch)>10:
+        mytime=epoch[11:]
+    else:
+        mytime='0'
+    fulldate=float(mydate+'.'+mytime)
+    x=datetime.fromtimestamp(fulldate)
+    return x.ctime()
+
+def decryptcookies(sqldb, destfile):
+    # convert sys argv into module parameters    
+    #sqldb=sys.argv[1]
+    #destfile=sys.argv[2]
+    # cookies file must be there, most often file name is cookies.sqlite
+    if not os.path.isfile(sqldb):
+        Usage()
+    # Bind to the sqlite db and execute sql statements
+    conn=sqlite3.connect(sqldb)
+    cur=conn.cursor()
+    try:
+        data=cur.execute('select * from moz_cookies')
+    except sqlite3.Error, e:
+        print 'Error {0}:'.format(e.args[0])
+        sys.exit(1)
+    mydata=data.fetchall()
+    # Dump results to a file
+    with open(destfile, 'w') as fp:
+        for item in mydata:
+            urlname=item[1]
+            urlname=item[1]
+            expiry=convert(str(item[8]))
+            accessed=convert(str(item[9]))
+            created=convert(str(item[10]))
+            fp.writelines(urlname + ',' + expiry + ',' + accessed + ',' + created)
+            fp.writelines('\n')
+    # Dump to stdout as well
+    with open(destfile) as fp:
+        for line in fp:
+            print line
+# END OF ADDENUM - decrypt cookies code
 
 
 if __name__ == "__main__":
     try:
         main()
-        time.sleep(10)
 
     except KeyboardInterrupt as e:
         print("Quit.")
-        time.sleep(10)
         sys.exit(Exit.KEYBOARD_INTERRUPT)
     except Exit as e:
         time.sleep(10)
         sys.exit(e.exitcode)
+
